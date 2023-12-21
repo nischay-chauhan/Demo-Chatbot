a5a62a02b99379be4c4a07733722f2b52ed5e270
nischay-chauhan <masternischay@outlook.com>
2023-12-21 17:50:07 +0530

    added the open AI key

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,35 +1,103 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import  { useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+
+import './App.css';
+import '@chatscope/chat-ui-kit-styles/dist/default/styles.min.css';
+import {
+  MainContainer,
+  ChatContainer,
+  MessageList,
+  Message,
+  MessageInput,
+  TypingIndicator
+} from '@chatscope/chat-ui-kit-react';
+
+const API_KEY = import.meta.env.REACT_APP_OPENAI_API_KEY;
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [message, setMessage] = useState([
+    {
+      message: "Hello I am Chatbot. Ask me any query",
+      sendTime: "just now",
+      sender: "bot",
+    },
+  ]);
+
+  const [typing, setTyping] = useState(false);
+
+  const handleSendRequest = async (message) => {
+    const newMessage = {
+      message,
+      direction: 'outgoing',
+      sender: 'user',
+    };
+    setMessage((prevMessage) => [...prevMessage, newMessage]);
+    setTyping(true);
+
+    try {
+      const response = await  processMessageToChatGPT([...message, newMessage]);
+      const content = response.choices[0]?.message?.content;
+
+      if (content) {
+        const chatGptResponse = {
+          message: content,
+          sender: "bot",
+        };
+        setMessage((prevMessage) => [...prevMessage, chatGptResponse]);
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setTyping(false);
+    }
+  };
+
+  async function processMessageToChatGPT(chatMessages) {
+    const apiMessages = chatMessages.map((messageObject) => {
+      const role = messageObject.sender === "ChatGPT" ? "assistant" : "user";
+      return { role, content: messageObject.message };
+    });
+
+    const apiRequestBody = {
+      model: "gpt-3.5-turbo",
+      messages: [
+        { role: "system", content: "I'm a Student using ChatGPT for learning" },
+        ...apiMessages,
+      ],
+    };
+
+    const response = await fetch("https://api.openai.com/v1/chat/completions", {
+      method: "POST",
+      headers: {
+        Authorization: "Bearer " + API_KEY,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(apiRequestBody),
+    });
+
+    return response.json();
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className='App'>
+      <div style={{ position: "relative", height: "800px", width: "700p" }}>
+        <MainContainer>
+          <ChatContainer>
+            <MessageList
+              scrollBehavior='smooth'
+              typingIndicator={typing ? <TypingIndicator content='Bot is typing' /> : null}
+            >
+              {message.map((msg) => (
+                <Message key={uuidv4()} model={msg} />
+              ))}
+            </MessageList>
+            <MessageInput placeholder='Send a message ' onSend={handleSendRequest} />
+          </ChatContainer>
+        </MainContainer>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
